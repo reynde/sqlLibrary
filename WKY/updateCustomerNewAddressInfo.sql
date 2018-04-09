d422dcb3897df24047f189ba774253380e4f5947
Roeland <rvandene@apexrnd.be>
2018-04-09 16:10:42 +0200

    no message

--- WKY/updateCustomerNewAddressInfo.sql
+++ WKY/updateCustomerNewAddressInfo.sql
@@ -1,49 +1,53 @@
-select *
-  from int_afterbuy_orders
- where orderid = 442266548
-  ;
-  
-  
-select id
-  from wky_customers
- where firstname = 'Maren'
-   and lastname = 'Werner'
-   and id = (
-            select ctr_id
-              from wky_orders
-             where ordernumber = '442266548'
-            )
- ;
- 
 
 
-update wky_customers
-   set deliveryaddressfirstname = 'rveTest'
-     , deliveryaddresslastname = 'rveTest'
-     , deliveryaddresstitle = 'rveTest'
-     , deliveryaddresscompany = 'rveTest'
-     , deliveryaddressstreetname2 = 'rveTest'
-     , deliveryaddressprovince = 'rveTest'
-     , deliveryaddressphonenumber = 'rveTest'
-     , deliveryaddressfax = 'rveTest'
-     , deliveryaddressmail = 'rveTest'
-     , billingaddressfirstname = 'rveTest'
-     , billingaddresslastname = 'rveTest'
-     , billingaddresstitle = 'rveTest'
-     , billingaddresscompany = 'rveTest'
-     , billingaddressstreetname2 = 'rveTest'
-     , billingaddressprovince = 'rveTest'
-     , billingaddressphonenumber = 'rveTest'
-  where id = (
-              select id
-                from wky_customers
-               where firstname = 'Maren'
-                 and lastname = 'Werner'
-                 and id = (
+
+            select count(*)
+              from int_afterbuy_orders
+             where 1=1
+               --and orderid = 442266548
+               and lower( street) <> lower( shippingstreet)
+             ;
+
+declare
+  l_cnt number := 0;
+begin
+  dbms_output.put_line('START UPDATE');
+  for r in (
+            select *
+              from int_afterbuy_orders
+             where lower( street) <> lower( shippingstreet)
+               -- and orderid = 442266548
+           )
+  loop
+    l_cnt := l_cnt + 1;
+    -- dbms_output.put_line('UPDATING ' || r.orderid);
+    update wky_customers
+       set deliveryaddressfirstname = r.shippingfirstname
+         , deliveryaddresslastname = r.shippinglastname
+         , deliveryaddresstitle = r.title
+         , deliveryaddresscompany = r.shippingcompany
+         , deliveryaddressstreetname2 = r.shippingstreet2
+         , deliveryaddressprovince = r.shippingstateorprovince
+         , deliveryaddressphonenumber = r.shippingphone
+         , billingaddressfax = r.fax
+         , billingaddressmail = r.mail
+         , billingaddressfirstname = r.firstname
+         , billingaddresslastname = r.lastname
+         , billingaddresstitle = r.title
+         , billingaddresscompany = r.company
+         , billingaddressstreetname2 = r.street2
+         , billingaddressprovince = r.stateorprovince
+         , billingaddressphonenumber = r.phone
+     where id = (
                           select ctr_id
                             from wky_orders
-                           where ordernumber = '442266548'
-                          )
-             )
-;
+                           where ordernumber = to_char( r.orderid)
+                );
+             
+  
+  end loop;
 
+  dbms_output.put_line( l_cnt || ' lines updated.');
+  dbms_output.put_line('END UPDATE');
+
+end;

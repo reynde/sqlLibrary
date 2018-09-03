22578bd44745b20c97edd1cf1d6e85a0b9f44f38
Roeland <rvandene@apexrnd.be>
2018-09-03 07:49:25 +0200

    no message

--- WKY/testExecuteScenariosDoubles.sql
+++ WKY/testExecuteScenariosDoubles.sql
@@ -1,4 +1,4 @@
-select * from wky_orders where ordernumber like '555123%';
+select * from wky_orders odr where trunc( odr.orderdate) = trunc( to_date( '30082018', 'DDMMYYYY'));
 
 declare
   l_new_odr_id number := 154725684035818041548158489396756762106 ; --154734434283314402269378160830077105721; -- 154432903844390098126853313626467129560;
@@ -9,8 +9,28 @@ begin
 end;
 
 
-
-select *
+-- 31-08-2018: 3
+select count(*)
   from wky_orders_potential_double
   ;
   
+declare 
+  l_date      date   := to_date( '30082018', 'DDMMYYYY');
+  l_cnt       number := 0;
+  
+begin
+
+  for r_dbl in (
+                 select odr.id
+                   from wky_orders odr
+                  where trunc( odr.orderdate) = trunc( l_date)
+               )
+  loop
+    l_cnt := l_cnt + 1;
+    -- wky_utilities.double_order_check( p_new_odr_id => l_new_odr_id);
+    
+  end loop;
+
+  dbms_output.put_line(l_cnt);
+  
+end;

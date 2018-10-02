2f3e252a819cb3541d349d5d096e451c31754799
Roeland <rvandene@apexrnd.be>
2018-10-02 15:19:34 +0200

    Remove all

--- WKY/alterTableOrders.sql
+++
@@ -1,4 +0,0 @@
-select sysdate from dual;
-
-alter table wky_orders
-add column group_of_goods varchar2(4000);
--- WKY/appIdOffset.sql
+++
@@ -1,6 +0,0 @@
-begin
-  apex_application_install.set_application_id(400);
-  apex_application_install.generate_offset;
-  apex_application_install.set_application_alias('WKYTST_SAD');
-end;
-/
